843f227ded893a5a8e6a268b125b5f4ff5d67d68
benbrookarsh <33601258+benbrookarsh@users.noreply.github.com>
2022-07-12 15:13:47 +0300

    scooby doo

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace BenYasminSqlDemoKatan.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "Scooby Doo")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
